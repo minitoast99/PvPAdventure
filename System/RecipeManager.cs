a7e99450a1010c9e331cc9b6eb91aab47fde6be0
Garmon <Ljsm0007@gmail.com>
2024-12-25 03:25:31 -0500

    RecipeManager: Introduce recipes for exchanging loot into alternatives

---
+++ System/RecipeManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PvPAdventure.System
+{
+    [Autoload]
+    public class RecipeManager : ModSystem
+    {
+        private readonly List<List<int>> _lootTables =
+        [
+            [ItemID.FlyingKnife, ItemID.DaedalusStormbow, ItemID.CrystalVileShard, ItemID.IlluminantHook],
+            [ItemID.ChainGuillotines, ItemID.DartRifle, ItemID.ClingerStaff, ItemID.PutridScent, ItemID.WormHook],
+            [ItemID.FetidBaghnakhs, ItemID.DartPistol, ItemID.SoulDrain, ItemID.FleshKnuckles, ItemID.TendonHook],
+            [ItemID.BreakerBlade, ItemID.ClockworkAssaultRifle, ItemID.LaserRifle, ItemID.FireWhip],
+            [
+                ItemID.GolemFist, ItemID.PossessedHatchet, ItemID.Stynger, ItemID.StaffofEarth, ItemID.HeatRay,
+                ItemID.SunStone, ItemID.EyeoftheGolem
+            ]
+        ];
+
+        public override void AddRecipes()
+        {
+            foreach (var lootTable in _lootTables)
+                CreateDuplicateDropRecipe(lootTable, 3);
+        }
+
+        private static void CreateDuplicateDropRecipe(List<int> lootTable, int amountOfMaterial)
+        {
+            for (int i = 0; i < lootTable.Count; i++)
+            {
+                for (int j = 0; j < lootTable.Count; j++)
+                {
+                    if (j == i)
+                        continue;
+
+                    Recipe.Create(lootTable[i])
+                        .AddIngredient(lootTable[j], amountOfMaterial)
+                        .AddTile(TileID.WorkBenches)
+                        .Register();
+                }
+            }
+        }
+    }
+}
