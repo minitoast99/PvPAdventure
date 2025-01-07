69ee856f7cca43a2269f4eb13573dd62da0358f6
James Puleo <james@jame.xyz>
2025-01-07 18:16:01 -0500

    AdventurePlayer: Move `PingCommand` inside

--- AdventurePlayer.cs
+++ AdventurePlayer.cs
@@ -400,6 +400,22 @@ public class AdventurePlayer : ModPlayer
         _pingPongCanary++;
     }
 
+    public class PingCommand : ModCommand
+    {
+        public override void Action(CommandCaller caller, string input, string[] args)
+        {
+            foreach (var player in Main.ActivePlayers)
+            {
+                var ping = player.GetModPlayer<AdventurePlayer>().Latency;
+                if (ping != null)
+                    caller.Reply($"{player.name}: {ping.Value.Milliseconds}ms");
+            }
+        }
+
+        public override string Command => "ping";
+        public override CommandType Type => CommandType.Console;
+    }
+
     public override string ToString()
     {
         return $"{Player.whoAmI}/{Player.name}/{DiscordUser?.Id}";
