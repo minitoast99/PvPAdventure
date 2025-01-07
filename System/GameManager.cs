acbfd653586b011edd400962d304e0a4d0154909
James Puleo <james@jame.xyz>
2025-01-07 18:30:03 -0500

    GameManager: Add `team` command

--- System/GameManager.cs
+++ System/GameManager.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Linq;
 using Terraria;
+using Terraria.Enums;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace PvPAdventure.System;
@@ -10,5 +14,31 @@ public class GameManager : ModSystem
     {
         // Prevent the world from entering the lunar apocalypse (killing cultist and spawning pillars)
         On_WorldGen.TriggerLunarApocalypse += _ => { };
+    }
+
+    public class TeamCommand : ModCommand
+    {
+        public override void Action(CommandCaller caller, string input, string[] args)
+        {
+            if (args.Length < 2)
+                return;
+
+            var player = Main.player
+                .Where(player => player.active)
+                .Where(player => player.name.Contains(args[0], StringComparison.CurrentCultureIgnoreCase))
+                .FirstOrDefault();
+
+            if (player == null)
+                return;
+
+            if (!Enum.TryParse(args[1], true, out Team team) || (int)team >= Enum.GetValues<Team>().Length)
+                return;
+
+            player.team = (int)team;
+            NetMessage.SendData(MessageID.PlayerTeam, number: player.whoAmI);
+        }
+
+        public override string Command => "team";
+        public override CommandType Type => CommandType.Console;
     }
 }
