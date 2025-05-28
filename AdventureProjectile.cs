2922e72d6c5f4e51ed56eb0bb71d256aa09568b3
EJCrispy <ej@soundstasty.com>
2025-05-28 16:50:11 -0700

    Makes the spac and claff projectiles disappear on dead

--- AdventureProjectile.cs
+++ AdventureProjectile.cs
@@ -446,4 +446,32 @@ public class AdventureProjectile : GlobalProjectile
             }
         }
     }
+
+    public class DeadProjectileList : GlobalProjectile
+    {
+        public override bool AppliesToEntity(Projectile entity, bool lateInstantiation)
+        {
+            return entity.type == ProjectileID.ClingerStaff ||
+                   entity.type == ProjectileID.SporeTrap ||
+                   entity.type == ProjectileID.SporeTrap2 ||
+                   entity.type == ProjectileID.SporeGas ||
+                   entity.type == ProjectileID.SporeGas2 ||
+                   entity.type == ProjectileID.SporeGas3;
+        }
+
+        public override void PostAI(Projectile projectile)
+        {
+            // Ensure owner index is valid
+            if (projectile.owner < 0 || projectile.owner >= Main.maxPlayers)
+                return;
+
+            Player owner = Main.player[projectile.owner];
+
+            // Kill projectile if owner is dead or inactive
+            if (owner.dead || !owner.active)
+            {
+                projectile.Kill();
+            }
+        }
+    }
 }
