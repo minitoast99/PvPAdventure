03f8566dccaa5ec055bc87357a1cd094c9174994
James Puleo <james@jame.xyz>
2025-06-07 02:25:43 -0400

    AdventureInventory: Make the hotbar translucent when mousing over

--- System/AdventureInventory.cs
+++ System/AdventureInventory.cs
@@ -1,9 +1,10 @@
 using System.Reflection;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
 using Terraria;
-using Terraria.ID;
+using Terraria.GameContent;
 using Terraria.ModLoader;
 using Terraria.UI;
 
@@ -16,6 +17,7 @@ public class AdventureInventory : ModSystem
         int slot, bool flag3, int xLoc, int yLoc, bool skipCheck);
 
     private Hook _accessorySlotLoaderDrawHook;
+    private float _hotbarColorModulate = 1.0f;
 
     public override void Load()
     {
@@ -36,6 +38,8 @@ public class AdventureInventory : ModSystem
         On_ItemSlot.RightClick_ItemArray_int_int += OnItemSlotRightClick;
         On_ItemSlot.LeftClick_ItemArray_int_int += OnItemSlotLeftClick;
         On_ItemSlot.MouseHover_ItemArray_int_int += OnItemSlotMouseHover;
+
+        IL_ItemSlot.Draw_SpriteBatch_ItemArray_int_int_Vector2_Color += EditItemSlotDraw;
     }
 
     private static bool IsPlayerDyeContext(int context) => context is ItemSlot.Context.EquipDye
@@ -101,6 +105,43 @@ public class AdventureInventory : ModSystem
             return;
 
         orig(inv, context, slot);
+    }
+
+    private void EditItemSlotDraw(ILContext il)
+    {
+        var cursor = new ILCursor(il);
+
+        // First, find the first store to the local which holds the alpha for hotbar inventory backing...
+        cursor.GotoNext(i => i.MatchStloc(15));
+        // ...and then find the next one...
+        cursor.GotoNext(i => i.MatchStloc(15));
+        // ...and go past it and a bit more for branching...
+        cursor.Index += 2;
+        // ...to prepare a delegate call, taking a ref to the alpha value.
+        cursor.EmitLdloca(15);
+        cursor.EmitDelegate((ref byte alpha) =>
+        {
+            const int numberOfHotbarSlots = 10;
+            // Close enough for all the items. We inflate anyhow.
+            const float hotbarScale = 0.75f;
+
+            // Approximate how big the hotbar area is going to be.
+            var bounding = new Rectangle(
+                20,
+                (int)(20f + 22f * (1f - hotbarScale)),
+                (int)((TextureAssets.InventoryBack.Width() * hotbarScale + 4) * numberOfHotbarSlots),
+                (int)(TextureAssets.InventoryBack.Height() * hotbarScale + 4)
+            );
+
+            // Fluff it a bit.
+            bounding.Inflate(30, 30);
+
+            // If the mouse is intersecting, start modulating down, otherwise modulate back up.
+            var target = bounding.Contains(Main.mouseX, Main.mouseY) ? 0.25f : 1.0f;
+            _hotbarColorModulate = (float)Utils.Lerp(_hotbarColorModulate, target, 1.0f / 128.0f);
+
+            alpha = (byte)(alpha * _hotbarColorModulate);
+        });
     }
 
     // FIXME: Ignore client syncing dye slots to server.
