f1ccca0e54c3d8b1680c7b31d5618dfb039f6ba1
James Puleo <james@jame.xyz>
2025-06-07 05:02:00 -0400

    PvPAdventure: Remove unused color static field

--- PvPAdventure.cs
+++ PvPAdventure.cs
@@ -13,8 +13,6 @@ namespace PvPAdventure;
 
 public class PvPAdventure : Mod
 {
-    internal static Color NpcChatColor = new(245, 133, 34);
-
     public override void Load()
     {
         // This mod should only ever be loaded when connecting to a server, it should never be loaded beforehand.
