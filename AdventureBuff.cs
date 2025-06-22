53cf98646fcbd694ef91e91cb310fd59db778cd8
Alex <87573461+MatteSevai@users.noreply.github.com>
2025-06-22 11:21:04 -0700

    remnoved duplicate code

--- AdventureBuff.cs
+++ AdventureBuff.cs
@@ -50,17 +50,7 @@ public class AdventureBuff : GlobalBuff
 
         return true;
     }
-    public class ShinyStoneHotswap : ModBuff
-    {
-        public override string Texture => $"PvPAdventure/Assets/Buff/ShinyStoneHotswap";
 
-        public override void SetStaticDefaults()
-        {
-            Main.debuff[Type] = true;
-            Main.buffNoSave[Type] = true;
-            Main.buffNoTimeDisplay[Type] = false; // Show timer
-        }
-    }
     public class RemoveFlaskBuffsOnDeath : ModPlayer
     {
         // Array of buff IDs to remove on death
